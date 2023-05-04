0095baa65ccc3d7f82f80f5006b932f174294be4
Yuji Iwanaga <nm7.ty.nt.abc@gmail.com>
2023-05-04 11:51:15 +0900

    ...

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,15 +2,22 @@
 #include "timer.h"
 #include <iostream>
 
+constexpr int USE_MICRO_S = 0;
+
 int main(int argc, char* argv[])
 {
   Timer timer;
 
-  for (auto i = 0; i < 5000; i++)
+  for (auto i = 0; i < 1000000; i++)
   {
     // loop
   }
 
+#if USE_MICRO_S
+  std::wcout << "Result : " << timer.elapsed_micro() << " micro s" << std::endl;
+#else
   std::wcout << "Result : " << timer.elapsed() << " ms" << std::endl;
+#endif
+
   return 0;
 }
